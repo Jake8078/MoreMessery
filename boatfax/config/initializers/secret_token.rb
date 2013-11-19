efe68afd5d9c0fc4ca2a9db266cdf500d7b70e24
jake8078 <jake8078@hotmail.com>
2013-11-18 21:01:14 -0500

    initial upload

---
+++ boatfax/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Boatfax::Application.config.secret_key_base = '2709ac573c03544c6911e7932fecd28c77066c6c56d7c7925364e849e542ff2c075f6465ee579ec80a06a477d8e4b5e7cac59c373ffe3ead211e94c6d2366641'
---
+++ boatfax/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Boatfax::Application.config.session_store :cookie_store, key: '_boatfax_session'
